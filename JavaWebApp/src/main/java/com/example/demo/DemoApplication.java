cadd75db1ce37b7af9363a44e3c0245886efec35
wilmatumenta <wilmayengwia@gmail.com>
2022-10-28 18:03:01 +0100

    modified for maven

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,11 @@
 package com.example.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-		string name ="wilma"
-		sytem.out.println(name);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        String name = "wilma";
+        System.out.println(name);
+    }
 }
